011261f812c3e1a4453bb2560f84fe97f90cbd10
ManarSayedEid <manarsayed1994@gmail.com>
2021-06-16 01:34:34 +0200

    fetch chat messages

--- src/components/layout/messenger/messenger.jsx
+++ src/components/layout/messenger/messenger.jsx
@@ -8,6 +8,8 @@ import "./messenger.css";
 
 const Messenger = ({ user }) => {
   const [conversations, setConversations] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState([]);
 
   // determine user => will be fetched from redux
   console.log(user);
@@ -26,6 +28,26 @@ const Messenger = ({ user }) => {
     };
     getConversations();
   }, [user]);
+
+  console.log(currentChat);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get(
+          "https://project-soul-api.herokuapp.com/api/messages/" +
+            currentChat._id
+        );
+        console.log(res.data);
+        setMessages(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMessages();
+  }, [currentChat]);
+  console.log(messages);
+
   return (
     <>
       <div className="messenger">
@@ -33,34 +55,47 @@ const Messenger = ({ user }) => {
           <div className="chatMenuWrapper">
             {conversations.map((conversation) => {
               return (
-                <Conversation
-                  conversationMembers={conversation}
-                  currentUser={user}
-                  key={conversation._id}
-                />
+                <div onClick={() => setCurrentChat(conversation)}>
+                  <Conversation
+                    conversationMembers={conversation}
+                    currentUser={user}
+                    key={conversation._id}
+                  />
+                </div>
               );
             })}
           </div>
         </div>
         <div className="chatBox">
           <div className="chatBoxWrapper">
-            <div className="chatBoxTop">
-              <Message own={true} />
-              <Message />
-              <Message />
-              <Message own={true} />
-              <Message />
-              <Message own={true} /> '
-            </div>
-            <div className="chatBoxBottom">
-              <textarea
-                rows="3"
-                placeholder="Write Some Thing..."
-                className="chatMessageInput"
-              ></textarea>
+            {currentChat ? (
+              <>
+                {" "}
+                <div className="chatBoxTop">
+                  {messages.map((message) => {
+                    return (
+                      <div>
+                        <Message
+                          message={message}
+                          own={message.Sender === user._id}
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="chatBoxBottom">
+                  <textarea
+                    rows="3"
+                    placeholder="Write Some Thing..."
+                    className="chatMessageInput"
+                  ></textarea>
 
-              <button className="chatSubmitButton">Send</button>
-            </div>
+                  <button className="chatSubmitButton">Send</button>
+                </div>{" "}
+              </>
+            ) : (
+              <span className="NoConversation"> Select Conversation</span>
+            )}
           </div>
         </div>
       </div>
