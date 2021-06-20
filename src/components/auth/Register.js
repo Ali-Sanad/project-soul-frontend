4e5f0359863962139b7386577e6f503e2218aa06
Ali Sanad <alisanad099@gmail.com>
2021-06-20 11:52:23 +0200

    user register UI

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -44,100 +44,112 @@ const Register = ({history, setAlert, register, auth}) => {
 
   return (
     <>
-      <div className=' mx-auto card rounded-lg  mt-6 mb-12 sm:w-4/5 md:w-1/2 lg:w-2/6 w-5/6'>
-        <div className='mx-auto mt-6 ml-12 w-4/5'>
-          <h4 className=' text-4xl bold my-2 text-blue-500'>Join Us </h4>
-          <p className='text-md'>Make the most out of your life</p>
-        </div>
-        <form className='mt-2' onSubmit={(e) => onSubmit(e)}>
-          <div className=' mt-6 '>
-            <input
-              className='block mx-auto mt-2  w-4/5 p-3 rounded-md
-            border border-gray-900  focus:outline-none
-            focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
+      <div className='grid sm:grid-cols-2 grid-cols-1 h-screen bg-soul_bg font-Nunito'>
+        <div className=''>
+          <div className='w-44 h-24 mx-auto'>
+            <img src='./images/logo.png' alt='' />
+          </div>
+          <h4 className='text-4xl bold  text-center'>Register</h4>
+          <form className='mt-2' onSubmit={(e) => onSubmit(e)}>
+            <div className=' mt-6 '>
+              <input
+                className='block mx-auto mt-2  w-4/5 p-3 rounded-full
+            border focus:outline-none
+            focus:ring-1 focus:to-soul focus:border-transparent 
             '
-              type='text'
-              placeholder='Name'
-              name='name'
-              value={name}
-              onChange={(e) => onChange(e)}
-              required
-            />
-          </div>
-          <div className='mt-6'>
-            <input
-              className='block mx-auto mt-6  w-4/5 p-3 rounded-md
-            border border-gray-900  focus:outline-none
-             focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
+                type='text'
+                placeholder='Name'
+                name='name'
+                value={name}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className='mt-6'>
+              <input
+                className='block mx-auto mt-2  w-4/5 p-3 rounded-full
+                border focus:outline-none
+                focus:ring-1 focus:to-soul focus:border-transparent 
              '
-              type='email'
-              placeholder='Email'
-              name='email'
-              value={email}
-              onChange={(e) => onChange(e)}
-              required
-            />
-          </div>
-          <div className='relative mx-auto  mt-6'>
-            <input
-              className='block mx-auto mt-6  w-4/5 p-3 rounded-md
-             border border-gray-900 focus:outline-none
-               focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
+                type='email'
+                placeholder='Email'
+                name='email'
+                value={email}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className='relative mx-auto  mt-6'>
+              <input
+                className='block mx-auto mt-2  w-4/5 p-3 rounded-full
+                border focus:outline-none 
+                focus:ring-1 focus:to-soul focus:border-transparent 
               '
-              type={passwordShown1 ? 'text' : 'password'}
-              placeholder='Password'
-              name='password'
-              minLength='6'
-              value={password}
-              onChange={(e) => onChange(e)}
-              required
-            />
-            <span
-              className=' absolute text-md text-blue-500  cursor-pointer
-              md:top-3 sm:right-16 md:right-14 lg:right-14   top-3   right-14'
-              aria-hidden='true'
-              onClick={() => togglePasswordVisiblity1()}
-            >
-              {passwordShown1 ? 'hide' : 'show'}
-            </span>
-          </div>
-          <div className='relative mx-auto  mt-6'>
-            <input
-              className='block mx-auto mt-6  w-4/5 p-3 rounded-md
-              border border-gray-900 focus:outline-none
-              focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
+                type={passwordShown1 ? 'text' : 'password'}
+                placeholder='Password'
+                name='password'
+                minLength='6'
+                value={password}
+                onChange={(e) => onChange(e)}
+                required
+              />
+              <span
+                className=' absolute text-md text-soul-300  cursor-pointer
+              md:top-4 sm:right-16 md:right-14 lg:right-20   top-4   right-20'
+                aria-hidden='true'
+                onClick={() => togglePasswordVisiblity1()}
+              >
+                {passwordShown1 ? 'hide' : 'show'}
+              </span>
+            </div>
+            <div className='relative mx-auto  mt-6'>
+              <input
+                className='block mx-auto mt-2  w-4/5 p-3 rounded-full
+                border focus:outline-none
+                focus:ring-1 focus:to-soul focus:border-transparent 
               '
-              type={passwordShown2 ? 'text' : 'password'}
-              placeholder='Confirm Password'
-              name='password2'
-              minLength='6'
-              value={password2}
-              onChange={(e) => onChange(e)}
-              required
-            />
-            <span
-              className=' absolute text-md text-blue-500  cursor-pointer
-             md:top-3 sm:right-16 md:right-14 lg:right-14   top-3   right-14'
-              aria-hidden='true'
-              onClick={() => togglePasswordVisiblity2()}
-            >
-              {passwordShown2 ? 'hide' : 'show'}
-            </span>
-          </div>
-          <input
-            type='submit'
-            className=' block mx-auto mt-12 mb-12  bg-blue-500 hover:bg-blue-800 w-4/5 p-4 rounded-full text-white
-           outline-none
-           shadow-sm cursor-pointer'
-            value='Register'
+                type={passwordShown2 ? 'text' : 'password'}
+                placeholder='Confirm Password'
+                name='password2'
+                minLength='6'
+                value={password2}
+                onChange={(e) => onChange(e)}
+                required
+              />
+              <span
+                className=' absolute text-md text-soul-300  cursor-pointer
+                md:top-4 sm:right-16 md:right-14 lg:right-20   top-4   right-20'
+                aria-hidden='true'
+                onClick={() => togglePasswordVisiblity2()}
+              >
+                {passwordShown2 ? 'hide' : 'show'}
+              </span>
+            </div>
+            <div className='mt-6 sm:ml-2'>
+              <input
+                type='submit'
+                className=' block  bg-soul-100 hover:bg-soul-300  sm:ml-10 md:ml-16 ml-16  py-2 px-4 rounded-full
+                 outline-none
+                 shadow-sm cursor-pointer'
+                value='Register'
+              />
+            </div>
+            <p className=' block sm:ml-12 md:ml-20 ml-16 mt-4  '>
+              Already on Soul?{' '}
+              <span className=' text-soul-300 font-bold '>
+                <Link to='/login'>Sign In</Link>
+              </span>
+            </p>
+          </form>
+        </div>
+
+        <div className=' h-screen sm:block hidden'>
+          <img
+            src='./images/user-register.png'
+            alt=''
+            className='h-screen object-contain'
           />
-        </form>
-        <p className=' block text-center mt-12 mb-12'>
-          Already on Soul?{' '}
-          <span className=' text-blue-500  font-bold '>
-            <Link to='/login'>Sign In</Link>
-          </span>
-        </p>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
           100: '#e2e2d5',
           200: '#888883',
         },
+        soul: {100: '#A4EBF3', 200: '#35D4E5', 300: '#07AFC2'},
+        soul_light: '#CCF2F4',
+        soul_background: '#F4F9F9',
+        soul_gray: '#AAAAAA',
+        soul_black: '#263238',
       },
       fontFamily: {
         Nunito: ['Nunito'],
