3c3dc93ecd9a03d6ccc3b0318d5999e40b283b1e
ManarSayedEid <manarsayed1994@gmail.com>
2021-06-20 03:47:05 +0200

    edit  article page

--- src/components/layout/articles/article.js
+++ src/components/layout/articles/article.js
@@ -1,3 +1,4 @@
+import Moment from "react-moment";
 import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { getArticles } from "../../../actions/article";
@@ -12,15 +13,43 @@ const Article = ({ getArticles, article }) => {
 
   return (
     <div className="container">
-      <div className="heroSection__article row">
+      <div className="heroSection__article">
         <div className="heroSection__article__text">
           <h4>Explore Our Articles</h4>
           <p>Take a quick review what our doctors say</p>
-          <button>Explore</button>
+          <a href="#articleScroll">Explore</a>
         </div>
-        <div className="heroSection__article__img">
-          <img src="\article.png" />
+        <div className="heroSection__article__image">
+          <img
+            className="heroSection__article__image__img"
+            src="/images/article.png"
+          />
         </div>
+      </div>
+      <div className="Articles__header">
+        <span id="articleScroll">Articles</span>
+      </div>
+      <div className="articles">
+        {article?.articles.map((article) => {
+          return (
+            <>
+              <div className="article">
+                <div>
+                  <img
+                    src={article.ArticleImg || "/images/defaultArticleImg.png"}
+                  />
+                </div>
+                <div>
+                  <small>
+                    <Moment format="YYYY/MM/DD">{article.date}</Moment>
+                  </small>
+                  <h4>{article.title}</h4>
+                  <p>{article.content}</p>
+                </div>
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import './index.css';
-import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import "./index.css";
+//import sass
+import "./sass/main.scss";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
