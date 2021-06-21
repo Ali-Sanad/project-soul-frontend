91bbc720b1e4c2e33557ed37ab9d67f62b632e2f
ManarSayedEid <manarsayed1994@gmail.com>
2021-06-21 05:24:37 +0200

    edit articles

--- src/components/layout/articles/article.js
+++ src/components/layout/articles/article.js
@@ -1,8 +1,14 @@
-import Moment from 'react-moment';
-import { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { getArticles } from '../../../actions/article';
-import { Link } from 'react-router-dom';
+import Moment from "react-moment";
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { getArticles } from "../../../actions/article";
+import { Link } from "react-router-dom";
+
+import Footer from "../../shared/footer";
+import NavBar from "../../shared/navbar";
+import Message from "../../shared/message";
+import ToTop from "../../shared/totop";
+
 //article
 const Article = ({ getArticles, article }) => {
   useEffect(() => {
@@ -12,46 +18,53 @@ const Article = ({ getArticles, article }) => {
   console.log(article);
 
   return (
-    <div className='container'>
-      <div className='heroSection__article'>
-        <div className='heroSection__article__text'>
-          <h2>Explore Our Articles</h2>
-          <p>Take a quick review what our doctors say</p>
-          <a href='#articleScroll'>Explore</a>
+    <div className="container-fluid">
+      <NavBar />
+      <div className="container">
+        <div className="heroSection__article">
+          <div className="heroSection__article__text">
+            <h3>Explore Our Articles</h3>
+            <p>Take a quick review what our doctors say</p>
+            <a href="#articleScroll">Explore</a>
+          </div>
+          <div className="heroSection__article__image">
+            <img
+              className="heroSection__article__image__img"
+              src="/images/article.png"
+            />
+          </div>
         </div>
-        <div className='heroSection__article__image'>
-          <img
-            className='heroSection__article__image__img'
-            src='/images/article.png'
-          />
+        <div className="Articles__header">
+          <span id="articleScroll">Articles</span>
         </div>
-      </div>
-      <div className='Articles__header'>
-        <span id='articleScroll'>Articles</span>
-      </div>
-      <div className='articles'>
-        {article?.articles.map((article) => {
-          return (
-            <>
-              <div className='article'>
-                <div>
-                  <img
-                    src={article.ArticleImg || '/images/defaultArticleImg.png'}
-                  />
+        <div className="articles">
+          {article?.articles.map((article) => {
+            return (
+              <>
+                <div className="article">
+                  <div>
+                    <img
+                      src={
+                        article.ArticleImg || "/images/defaultArticleImg.png"
+                      }
+                    />
+                  </div>
+                  <div>
+                    <small>
+                      <Moment format="YYYY/MM/DD">{article.date}</Moment>
+                    </small>
+                    <h4>{article.title}</h4>
+                    <p>{article.content}</p>
+                  </div>
                 </div>
-                <div>
-                  <small>
-                    <Moment format='YYYY/MM/DD'>{article.date}</Moment>
-                  </small>
-                  <h4>{article.title}</h4>
-                  <p>{article.content}</p>
-                </div>
-              </div>
-            </>
-          );
-        })}
+              </>
+            );
+          })}
+        </div>
       </div>
       <Footer />
+      <Message></Message>
+      <ToTop></ToTop>
     </div>
   );
 };
