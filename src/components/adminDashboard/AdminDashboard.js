b99d836d874c81882d517d8a3d9092f8ff4ebb97
Ali Sanad <alisanad099@gmail.com>
2021-06-22 01:32:49 +0200

    user & admin & therapist login fix

--- src/components/adminDashboard/AdminDashboard.js
+++ src/components/adminDashboard/AdminDashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AdminDashboard = () => {
   return (
-    <div className='text-center font-bold  text-lg font-Nunito mt-24'>
+    <div className='text-center font-bold text-soul-300  text-lg font-Nunito mt-24'>
       Admin Dashboard
     </div>
   );
