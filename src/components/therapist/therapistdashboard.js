caa616ffdaff5bfc5b4387537367a8a06ad84c42
Ali Sanad <alisanad099@gmail.com>
2021-06-23 23:08:46 +0200

    fix

--- src/components/therapist/therapistdashboard.js
+++ src/components/therapist/therapistdashboard.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
-import Navbar from "../shared/navbar";
-import SideNav from "../shared/sidenav";
+import Navbar from '../shared/navbar';
+import SideNav from '../shared/sidenav';
 import TherapistFiles from './therapistfiles';
-import TherapistProfile from "./therapistprofile"
+import TherapistProfile from './therapistprofile';
 import TherapistSummary from './therapistsummary';
 
 const TherapistDashboard = () => {
-	return (
-		<React.Fragment>
-			<div className="therapistdashboard">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <Navbar className="therapistdashboard__navbar"></Navbar>
-                    </div>
-                    <div className="col-3">
-                        <SideNav></SideNav>
-                    </div>
-                    <div className="col-8">
-                        {/* <TherapistFiles></TherapistFiles> */}
-                        {/* <TherapistProfile></TherapistProfile> */}
-                        <TherapistSummary></TherapistSummary>
-                    </div>
-                </div>
+  return (
+    <React.Fragment>
+      <div className='therapistdashboard'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12'>
+              <Navbar className='therapistdashboard__navbar'></Navbar>
             </div>
-			</div>
-		</React.Fragment>
-	);
+            <div className='col-3'>
+              <SideNav></SideNav>
+            </div>
+            <div className='col-8'>
+              <TherapistFiles></TherapistFiles>
+              <TherapistProfile></TherapistProfile>
+              <TherapistSummary></TherapistSummary>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default TherapistDashboard;
