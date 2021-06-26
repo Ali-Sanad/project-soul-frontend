93a32503e66fe548f1ee07912d3a6e05958891a5
unknown <asmaaeldeeb04@gmail.com>
2021-06-26 14:18:57 +0200

    post

--- src/components/posts/SearchPost.js
+++ src/components/posts/SearchPost.js
@@ -1,13 +1,43 @@
-import React ,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ImageSearch from '../../assets/images/icons8_Search_4 3.png';
 import { Dropdown } from 'react-bootstrap';
-const SearchPost = () => {
-    const [searchValue, setSearch] = useState('');
-    return ( 
-        <>
-       <div className="postForm">
-       <div className="container">
-            <div className="row">
+import { getPosts } from '../../actions/post';
+import Spinner from './Spinner';
+import { connect } from 'react-redux';
+import PostCard from './PostList';
+
+const SearchPost = ({
+  getPosts,
+  auth: { user, isAuthenticated },
+  post: { posts, loading },
+}) => {
+  const [searchValue, setSearch] = useState('');
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  let searchPosts = !loading ? (
+    posts
+      .filter((post) => {
+        if (searchValue == '') {
+          return post;
+        } else if (
+          post.text.toLowerCase().includes(searchValue.toLowerCase())
+        ) {
+          return post;
+        }
+      })
+      .map((post) => {
+        <PostCard key={post._id} post={post} />;
+      })
+  ) : (
+    <Spinner />
+  );
+  return (
+    <>
+      <div className='postForm'>
+        <div className='container'>
+          <div className='row'>
             <div className='col-lg-12 postForm__inputs'>
               {/* <Dropdown>
                 <Dropdown.Toggle variant='success' id='dropdown-basic'>
@@ -30,18 +60,25 @@ const SearchPost = () => {
                 className='postForm__inputs__search inputstyle'
                 value={searchValue}
                 onChange={(e) => setSearch(e.target.value)}
-                  required
+                required
               />
               <a>
                 <img src={ImageSearch} className='postForm__imageSearch' />
               </a>
+              {searchPosts}
             </div>
-            </div>
-
+          </div>
         </div>
-       </div>
-        </>
-     );
-}
- 
-export default SearchPost;
+      </div>
+    </>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    post: state.post,
+    auth: state.auth,
+  };
+};
+export default connect(mapStateToProps, {
+  getPosts,
+})(SearchPost);
