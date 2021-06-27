15762e494f9ff8c0f82d65a9bfd05a00c1a49452
yasmin <yasminfathy520@gmail.com>
2021-06-27 19:43:00 +0200

    reset-password

--- src/components/auth/ConfirmTherapistAccount/TherapistForgotPassword.js
+++ src/components/auth/ConfirmTherapistAccount/TherapistForgotPassword.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { forgotPassword } from "../../../actions/therapistAuth";
+import logo from "../../../assets/images/logo.png";
+
 const TherapistForgotPassword = ({ forgotPassword, auth }) => {
   const [email, setEmail] = useState("");
 
@@ -16,32 +18,33 @@ const TherapistForgotPassword = ({ forgotPassword, auth }) => {
   };
 
   return (
-    <div className=" mx-auto card rounded-lg  mt-24 mb-12 sm:w-4/5 md:w-1/2 lg:w-2/6 w-5/6  ">
+    <div className=" w-2/3 lg:w-1/3 mt-24 mx-auto  h-screen bg-soul_bg font-Nunito ">
       {auth.note ? (
         <>
-          <div className="mx-auto mt-6 ml-12 w-4/5">
-            <h4 className=" text-xl bold my-2 text-center text-blue-500">
+          <div className="mx-auto mt-25  w-4/5 font-Nunito">
+            <h4 className=" text-xl bold my-2 text-center ">
               An email has been sent to{" "}
-              <span className="text-green-600">{email} </span>, check your inbox
+              <span className="text-soul-300">{email} </span>, check your inbox
               and reset your password !
             </h4>
           </div>
         </>
       ) : (
         <>
-          <div className="mx-auto mt-6 ml-12 w-4/5">
-            <h4 className=" text-4xl bold my-2 text-center text-blue-500">
-              Forgot Password
-            </h4>
+          <div className="w-44 h-24 mx-auto">
+            <Link to="/">
+              <img src={logo} alt="" />
+            </Link>
           </div>
+          <h4 className="text-2xl   text-center"> Forgot Password</h4>
 
-          <form className="mt-6 " onSubmit={(e) => onSubmit(e)}>
-            <div className=" mt-6 ">
+          <form className="mt-6 mx-auto" onSubmit={(e) => onSubmit(e)}>
+            <div className=" mt-6">
               <input
-                className="block mx-auto mt-6  w-4/5 p-3 rounded-md
-            border border-gray-900  focus:outline-none
-             focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
-            "
+                className="block mx-auto mt-2  w-4/5 p-3 rounded-full
+              border focus:outline-none
+              focus:ring-1 focus:to-soul focus:border-transparent 
+          "
                 type="email"
                 placeholder="Email"
                 name="email"
@@ -50,26 +53,81 @@ const TherapistForgotPassword = ({ forgotPassword, auth }) => {
                 required
               />
             </div>
-            <input
-              type="submit"
-              className=" block mx-auto mt-6 mb-6  bg-blue-500 hover:bg-blue-800 w-4/5 p-4 rounded-full text-white
-            outline-none
-            shadow-sm cursor-pointer"
-              value="Send"
-            />
-          </form>
-
-          <p className=" block text-center mt-12 mb-12">
-            New to Soul?
-            <span className=" text-blue-500  font-bold ">
-              <Link to="/register">
-                <span> </span>Join now
+            <div className="mt-8  ">
+              <input
+                type="submit"
+                className=" block bg-soul-100 mx-auto hover:bg-soul-300 py-2 px-4 rounded-full
+              outline-none
+              shadow-sm cursor-pointer"
+                value="Send"
+              />
+            </div>
+            <p className=" mt-6 text-center  cursor-pointer">
+              <Link
+                to="/registertherapist"
+                className=" text-black no-underline hover:text-soul-300"
+              >
+                Don't have an account ?
               </Link>
-            </span>
-          </p>
+            </p>
+          </form>
         </>
       )}
     </div>
+    // <div className=" mx-auto card rounded-lg  mt-24 mb-12 sm:w-4/5 md:w-1/2 lg:w-2/6 w-5/6  ">
+    //   {auth.note ? (
+    //     <>
+    //       <div className="mx-auto mt-6 ml-12 w-4/5">
+    //         <h4 className=" text-xl bold my-2 text-center text-blue-500">
+    //           An email has been sent to{" "}
+    //           <span className="text-green-600">{email} </span>, check your inbox
+    //           and reset your password !
+    //         </h4>
+    //       </div>
+    //     </>
+    //   ) : (
+    //     <>
+    //       <div className="mx-auto mt-6 ml-12 w-4/5">
+    //         <h4 className=" text-4xl bold my-2 text-center text-blue-500">
+    //           Forgot Password
+    //         </h4>
+    //       </div>
+
+    //       <form className="mt-6 " onSubmit={(e) => onSubmit(e)}>
+    //         <div className=" mt-6 ">
+    //           <input
+    //             className="block mx-auto mt-6  w-4/5 p-3 rounded-md
+    //         border border-gray-900  focus:outline-none
+    //          focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
+    //         "
+    //             type="email"
+    //             placeholder="Email"
+    //             name="email"
+    //             value={email}
+    //             onChange={(e) => onChange(e)}
+    //             required
+    //           />
+    //         </div>
+    //         <input
+    //           type="submit"
+    //           className=" block mx-auto mt-6 mb-6  bg-blue-500 hover:bg-blue-800 w-4/5 p-4 rounded-full text-white
+    //         outline-none
+    //         shadow-sm cursor-pointer"
+    //           value="Send"
+    //         />
+    //       </form>
+
+    //       <p className=" block text-center mt-12 mb-12">
+    //         New to Soul?
+    //         <span className=" text-blue-500  font-bold ">
+    //           <Link to="/register">
+    //             <span> </span>Join now
+    //           </Link>
+    //         </span>
+    //       </p>
+    //     </>
+    //   )}
+    // </div>
   );
 };
 TherapistForgotPassword.propTypes = {
@@ -78,7 +136,7 @@ TherapistForgotPassword.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth,
+  auth: state.therapistAuth,
 });
 
 export default connect(mapStateToProps, { forgotPassword })(
--- src/components/auth/ConfirmTherapistAccount/TherapistResetPassword.js
+++ src/components/auth/ConfirmTherapistAccount/TherapistResetPassword.js
@@ -1,25 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import {resetPassword} from '../../../actions/therapistAuth';
-import {Redirect} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { resetPassword } from "../../../actions/therapistAuth";
+import { Redirect } from "react-router-dom";
+import logo from "../../../assets/images/logo.png";
 
-import {setTherapistAuthToken} from '../../../utils/setAuthToken';
-
-const TherapistResetPassword = ({match, resetPassword, auth}) => {
+import { setTherapistAuthToken } from "../../../utils/setAuthToken";
+import { Link } from "react-router-dom";
+const TherapistResetPassword = ({ match, resetPassword, auth }) => {
   useEffect(() => {
     setTherapistAuthToken(match.params.id);
   }, [match.params.id]);
 
   const [formData, setFormData] = useState({
-    password: '',
-    confirmPassword: '',
+    password: "",
+    confirmPassword: "",
   });
 
   const [passwordShown1, setPasswordShown1] = useState(false);
   const [passwordShown2, setPasswordShown2] = useState(false);
 
-  const {password, confirmPassword} = formData;
+  const { password, confirmPassword } = formData;
 
   const togglePasswordVisiblity1 = () => {
     setPasswordShown1(!passwordShown1);
@@ -29,83 +30,150 @@ const TherapistResetPassword = ({match, resetPassword, auth}) => {
   };
 
   const onChange = (e) => {
-    setFormData({...formData, [e.target.name]: e.target.value});
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
   const onSubmit = (e) => {
     e.preventDefault();
-    resetPassword(match.params.id, {password, confirmPassword});
+    resetPassword(match.params.id, { password, confirmPassword });
   };
-  console.log('auth', auth);
+  console.log("auth", auth);
   if (auth.redirect === true) {
-    return <Redirect to='/logintherapist' />;
+    return <Redirect to="/logintherapist" />;
   }
 
   return (
     <>
-      <div className=' mx-auto card rounded-lg  mt-24 mb-12 sm:w-4/5 md:w-1/2 lg:w-2/6 w-5/6  '>
-        <div className='mx-auto mt-6 ml-12 w-4/5'>
-          <h4 className=' text-4xl bold my-2 text-blue-500'>Reset Password</h4>
+      <div className=" w-3/4 md:w-2/3 lg:w-1/3 mt-24 mx-auto  h-screen bg-soul_bg font-Nunito">
+        <div className="w-44 h-24 mx-auto">
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
         </div>
+        <h4 className="text-2xl   text-center"> Reset Password</h4>
 
-        <form className='mt-6 ' onSubmit={(e) => onSubmit(e)}>
-          {/* password field and toggle password visibility */}
-          <div className='relative mx-auto  mt-6'>
+        <form className="mt-6 " onSubmit={(e) => onSubmit(e)}>
+          <div className="relative mx-auto  mt-6">
             <input
-              className='block mx-auto mt-6  w-4/5 p-3 rounded-md
+              className="block mx-auto mt-6  w-4/5 p-3 rounded-md
             border border-gray-900 focus:outline-none
               focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
-             '
-              type={passwordShown1 ? 'text' : 'password'}
-              placeholder='Password'
-              name='password'
-              minLength='6'
+             "
+              type={passwordShown1 ? "text" : "password"}
+              placeholder="Password"
+              name="password"
+              minLength="6"
               value={password}
               onChange={(e) => onChange(e)}
               required
             />
             <span
-              className=' absolute text-md text-blue-500  cursor-pointer
-            md:top-3 sm:right-16 md:right-14 lg:right-14   top-3   right-14'
-              aria-hidden='true'
+              className=" absolute text-md text-blue-500  cursor-pointer
+            md:top-3 sm:right-16 md:right-14 lg:right-14   top-3   right-14"
+              aria-hidden="true"
               onClick={() => togglePasswordVisiblity1()}
             >
-              {passwordShown1 ? 'hide' : 'show'}
+              {passwordShown1 ? "hide" : "show"}
             </span>
           </div>
 
-          <div className='relative mx-auto  mt-6'>
+          <div className="relative mx-auto  mt-6">
             <input
-              className='block mx-auto mt-6  w-4/5 p-3 rounded-md
+              className="block mx-auto mt-6  w-4/5 p-3 rounded-md
             border border-gray-900 focus:outline-none
               focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
-             '
-              type={passwordShown2 ? 'text' : 'password'}
-              placeholder='confirm Password'
-              name='confirmPassword'
-              minLength='6'
+             "
+              type={passwordShown2 ? "text" : "password"}
+              placeholder="confirm Password"
+              name="confirmPassword"
+              minLength="6"
               value={confirmPassword}
               onChange={(e) => onChange(e)}
               required
             />
             <span
-              className=' absolute text-md text-blue-500  cursor-pointer
-            md:top-3 sm:right-16 md:right-14 lg:right-14   top-3   right-14'
-              aria-hidden='true'
+              className=" absolute text-md text-blue-500  cursor-pointer
+            md:top-3 sm:right-16 md:right-14 lg:right-14   top-3   right-14"
+              aria-hidden="true"
               onClick={() => togglePasswordVisiblity2()}
             >
-              {passwordShown2 ? 'hide' : 'show'}
+              {passwordShown2 ? "hide" : "show"}
+            </span>
+          </div>
+
+          <div className="mt-8  ">
+            <input
+              type="submit"
+              className=" block bg-soul-100 mx-auto hover:bg-soul-300 py-2 px-4 rounded-full
+                outline-none
+                shadow-sm cursor-pointer"
+              value="Submit"
+            />
+          </div>
+        </form>
+      </div>
+      {/* <div className=" mx-auto card rounded-lg  mt-24 mb-12 sm:w-4/5 md:w-1/2 lg:w-2/6 w-5/6  ">
+        <div className="mx-auto mt-6 ml-12 w-4/5">
+          <h4 className=" text-4xl bold my-2 text-blue-500">Reset Password</h4>
+        </div>
+
+        <form className="mt-6 " onSubmit={(e) => onSubmit(e)}>
+          <div className="relative mx-auto  mt-6">
+            <input
+              className="block mx-auto mt-6  w-4/5 p-3 rounded-md
+            border border-gray-900 focus:outline-none
+              focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
+             "
+              type={passwordShown1 ? "text" : "password"}
+              placeholder="Password"
+              name="password"
+              minLength="6"
+              value={password}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <span
+              className=" absolute text-md text-blue-500  cursor-pointer
+            md:top-3 sm:right-16 md:right-14 lg:right-14   top-3   right-14"
+              aria-hidden="true"
+              onClick={() => togglePasswordVisiblity1()}
+            >
+              {passwordShown1 ? "hide" : "show"}
+            </span>
+          </div>
+
+          <div className="relative mx-auto  mt-6">
+            <input
+              className="block mx-auto mt-6  w-4/5 p-3 rounded-md
+            border border-gray-900 focus:outline-none
+              focus:ring-2 focus:ring-blue-400 focus:border-transparent shadow-sm
+             "
+              type={passwordShown2 ? "text" : "password"}
+              placeholder="confirm Password"
+              name="confirmPassword"
+              minLength="6"
+              value={confirmPassword}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <span
+              className=" absolute text-md text-blue-500  cursor-pointer
+            md:top-3 sm:right-16 md:right-14 lg:right-14   top-3   right-14"
+              aria-hidden="true"
+              onClick={() => togglePasswordVisiblity2()}
+            >
+              {passwordShown2 ? "hide" : "show"}
             </span>
           </div>
 
           <input
-            type='submit'
-            className=' block mx-auto mt-12 mb-12  bg-blue-500 hover:bg-blue-800 w-4/5 p-4 rounded-full text-white
+            type="submit"
+            className=" block mx-auto mt-12 mb-12  bg-blue-500 hover:bg-blue-800 w-4/5 p-4 rounded-full text-white
             outline-none
-            shadow-sm cursor-pointer'
-            value='Submit'
+            shadow-sm cursor-pointer"
+            value="Submit"
           />
         </form>
-      </div>
+      </div> */}
     </>
   );
 };
@@ -118,6 +186,6 @@ const mapStateToProps = (state) => ({
   auth: state.therapistAuth,
 });
 
-export default connect(mapStateToProps, {resetPassword})(
+export default connect(mapStateToProps, { resetPassword })(
   TherapistResetPassword
 );
