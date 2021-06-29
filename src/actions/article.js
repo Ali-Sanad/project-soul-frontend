5646cf71936a6d23d7f504caa8728b62ba457281
yasmin <yasminfathy520@gmail.com>
2021-06-29 21:01:39 +0200

    pull

--- src/actions/article.js
+++ src/actions/article.js
@@ -7,19 +7,16 @@ import {
   GET_ARTICLE,
   DELETE_ARTICLE,
 } from "./types";
-import { setAlert } from "./alert";
 
 //GET ALL ARTICLES
 export const getArticles = () => async (dispatch) => {
   try {
     const res = await axios.get("/article");
-    console.log("array of articles from article action", res.data);
     dispatch({
       type: GET_ARTICLES,
       payload: res.data,
     });
   } catch (error) {
-    console.log(error);
     dispatch({
       type: ARTICLES_ERROR,
     });
@@ -29,13 +26,11 @@ export const getArticles = () => async (dispatch) => {
 export const addArticle = (body) => async (dispatch) => {
   try {
     const res = await axios.post("/article", body);
-    console.log(res.data);
     dispatch({
       type: ADD_ARTICLE,
       payload: res.data,
     });
   } catch (error) {
-    console.log(error);
     dispatch({
       type: ARTICLES_ERROR,
     });
@@ -45,13 +40,11 @@ export const addArticle = (body) => async (dispatch) => {
 export const getArticle = (id) => async (dispatch) => {
   try {
     const res = await axios.get(`/article/${id}`);
-    console.log(res.data);
     dispatch({
       type: GET_ARTICLE,
       payload: res.data,
     });
   } catch (error) {
-    console.log(error);
     dispatch({
       type: ARTICLES_ERROR,
     });
@@ -61,14 +54,11 @@ export const getArticle = (id) => async (dispatch) => {
 export const deleteArticle = (id) => async (dispatch) => {
   try {
     await axios.delete("/article/" + id);
-    // console.log(res.data);
     dispatch({
       type: DELETE_ARTICLE,
       payload: id,
     });
-    // toast.warn("post deleted");
   } catch (error) {
-    console.log(error);
     dispatch({
       type: ARTICLES_ERROR,
     });
